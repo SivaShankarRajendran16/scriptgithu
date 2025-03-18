63b6bcafa438077959e2a744275906bedac5320b
SivaShankarRajendran16 <rajendransivashankar@gmail.com>
2025-03-18 13:53:05 +0530

    First commit

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "🚀 Bash script is running..."
+
+# Create a directory
+mkdir -p my_folder
+
+# Create a file inside the folder
+echo "Hello from Bash script!" > my_folder/sample.txt
+
+# List files
+echo "📂 Listing files:"
+ls -la my_folder
+
+echo "✅ Script execution finished!"
+
